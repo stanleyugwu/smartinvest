7d803b76cdb3b074e856f2a6ee0ff19292dd1914
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-08-25 18:14:28 +0100

    fix: resolve home page scripts execution inconsistency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./assets/js/GetButton";
@@ -6,16 +6,61 @@ import GoogleTranslate from "./components/GoogleTranslate";
 import WinnerCard from "./components/WinnerCard";
 import "./assets/js/GetButton";
 import Home from "./pages/home";
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route, Outlet, useLocation } from "react-router-dom";
 import SignupAndLogin from "./pages/signup_and_login";
 import AuthProvider from "./components/auth_provider/AuthProvider";
 import Dashboard from "./pages/dashboard";
 import RequireAuth from "./components/RequireAuth";
 
+const homePageInit = () => {
+   // @ts-ignore
+   let appInfo = App_Info(jQuery, window, document); // get app info object
+   // @ts-ignore
+   appInfo = Script_Init(appInfo, jQuery, window, document); // run scripts.js initialisation
+   // @ts-ignore
+   var AppInfo = __CHART_INIT(appInfo, jQuery, window); // run charts.js initialisation
+   AppInfo.Chart.ChartJs(); // draw chart
+   // below function will be called whenever user navigates to and from home page to trigger 
+   // scripts execution for normalising page
+   //  assigning it to variable won't work so we attach it to window object
+   // it will be called when user moves to and from home page
+   // @ts-ignore
+   if(window.$__onLoad) window.$__onLoad?.()
+   // @ts-ignore
+   else window.$__onLoad = appInfo?.winLoad;
+}
+
 /**
  * The home page is a spa, so elements are arranged accordingly
  */
 function App() {
+  useEffect(() => {
+    
+    /**
+     * Below undefined variables should be made available by `charts.js`, `scripts.js`,
+     * and `jquery` library.
+     * They're hopeully loaded in follwowing order: jquery.js, scripts.js, charts.js.
+     *
+     * The scripts: `scripts.js` and `charts.js` both have initialisation logics and they're supposed to
+     * automatically when the scripts are loaded. We've however prevented those initialisations until our app
+     * renders because the scripts will be loaded/executed before our app renders
+     */
+    try {
+     homePageInit()
+    } catch (error) {
+      console.log(error);
+      console.log("ERROR WITH THREE SCRIPTS LOADED IN INDEX.HTML");
+    }
+  }, []);
+
+  const {pathname} = useLocation()
+  useEffect(() => {
+    if(pathname === "/"){
+      // we re-executes our scripts everytime user goes to home
+      homePageInit();
+    }
+  }, [pathname])
+  
   return (
     <AuthProvider>
       <div className="App">
@@ -30,7 +75,7 @@ function App() {
             <Route path="/signin" element={<SignupAndLogin />} />
           </Route>
           <Route
-            path="/dashboard"
+            path="/home"
             element={
               <RequireAuth>
                 <Dashboard />
