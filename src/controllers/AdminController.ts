1baed6a28b16ec6f204953f6c964cb1f0b53cb93
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-09-10 21:24:09 +0100

    feat: create update account and find usr controllers

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -1,11 +1,89 @@
 import { Request, Response } from "express";
-import Admin from "../models/Admin";
 import User from "../models/User";
 import { sendErrorResponse, sendSuccessResponse } from "../modules/utils";
 import StatusCode from "../status";
 
+export type UserAccountUpdate = {
+  deposit: number;
+  balance: number;
+  profit: number;
+  credit: number;
+  withdrawal: number;
+  accountManager: string;
+  tradingPercentage: number;
+};
+
 class AdminController {
-  async updateAccount(req: Request, res: Response) {}
+  async updateUserAccount(
+    req: Request<any, any, UserAccountUpdate, { userId: string }>,
+    res: Response
+  ) {
+    // validate userID
+    const userId = req.query.userId;
+    if (!userId || !userId.trim()) {
+      return sendErrorResponse(
+        res,
+        "Missing User ID",
+        StatusCode.BAD_REQUEST,
+        "You didn't provide the ID of user to approve"
+      );
+    }
+
+    // validate update values
+    const {
+      accountManager,
+      balance,
+      credit,
+      deposit,
+      profit,
+      tradingPercentage,
+      withdrawal,
+    } = req.body;
+    if (
+      !accountManager &&
+      !balance &&
+      !credit &&
+      !deposit &&
+      !profit &&
+      !tradingPercentage &&
+      !withdrawal
+    ) {
+      return sendErrorResponse(
+        res,
+        "Empty update fields",
+        StatusCode.BAD_REQUEST,
+        "You supplied empty updte values"
+      );
+    }
+
+    try {
+      const user = await User.findByPk(userId);
+      if (!user)
+        return sendErrorResponse(
+          res,
+          "User Not Found",
+          StatusCode.NOT_FOUND,
+          "User with given ID doesn't exist"
+        );
+
+      // update user account
+      const updatedUser = await user.update({
+        ...user.toJSON(),
+        ...req.body
+      });
+
+      return sendSuccessResponse(
+        res,
+        {
+          profile: updatedUser.getProfileInfo(),
+          account: updatedUser.getAccountInfo(),
+        },
+        "User Account Updated Successfully"
+      );
+    } catch (error: any) {
+      return sendErrorResponse(res, error.message, StatusCode.BAD_REQUEST);
+    }
+  }
 
   async countApproveUsers(req: Request, res: Response) {
     try {
@@ -158,7 +236,48 @@ class AdminController {
       return sendErrorResponse(res, error.message, StatusCode.BAD_REQUEST);
     }
   }
-  findUserByEmail(req: Request, res: Response) {}
+  async findUserByEmail(
+    req: Request<any, any, any, { userEmail: string }>,
+    res: Response
+  ) {
+    const userEmail = req.query.userEmail;
+    console.log(userEmail);
+
+    if (!userEmail || !userEmail.trim()) {
+      return sendErrorResponse(
+        res,
+        "Missing User Email",
+        StatusCode.BAD_REQUEST,
+        "You didn't provide the e-mail address of user to find"
+      );
+    }
+
+    try {
+      const user = await User.findOne({
+        where: {
+          email: userEmail,
+        },
+      });
+      if (!user)
+        return sendErrorResponse(
+          res,
+          "User Not Found",
+          StatusCode.NOT_FOUND,
+          "User with given e-mail address doesn't exist"
+        );
+
+      return sendSuccessResponse(
+        res,
+        {
+          user: { ...user.getProfileInfo(), approved: user.approved },
+          account: user.getAccountInfo(),
+        },
+        "User Found"
+      );
+    } catch (error: any) {
+      return sendErrorResponse(res, error.message, StatusCode.BAD_REQUEST);
+    }
+  }
   async getLast30ApprovedUsers(req: Request, res: Response) {
     try {
       const users = await User.findAll({
