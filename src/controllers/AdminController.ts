047bd77ba7bf3f4ee208ee16b95ff0a9e81ae1d9
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-09-10 15:41:18 +0100

    feat: crete controllers for counting approved, unapproved, and all users

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -1,14 +1,41 @@
 import { Request, Response } from "express";
 import Admin from "../models/Admin";
+import User from "../models/User";
+import { sendErrorResponse, sendSuccessResponse } from "../modules/utils";
 
 class AdminController {
   updateAccount(req: Request, res: Response) {}
 
-  countApproveUsers(req: Request, res: Response) {}
+  async countApproveUsers(req: Request, res: Response) {
+    try {
+      const numOfApprvdUsers = await User.count({where:{
+        approved:true
+      }});
+      return sendSuccessResponse(res,numOfApprvdUsers,"Retrieval Successful")
+    } catch (error:any) {
+      return sendErrorResponse(res, error.message)
+    }
+  }
 
-  countUnapprovedUsers(req: Request, res: Response) {}
+  async countUnapprovedUsers(req: Request, res: Response) {
+    try {
+      const numOfUnApprvdUsers = await User.count({where:{
+        approved:false
+      }});
+      return sendSuccessResponse(res,numOfUnApprvdUsers,"Retrieval Successful")
+    } catch (error:any) {
+      return sendErrorResponse(res, error.message)
+    }
+  }
 
-  countAllUsers(req: Request, res: Response) {}
+  async countAllUsers(req: Request, res: Response) {
+    try {
+      const numOfUsers = await User.count();
+      return sendSuccessResponse(res,numOfUsers,"Retrieval Successful")
+    } catch (error:any) {
+      return sendErrorResponse(res, error.message)
+    }
+  }
   approveUser(req: Request, res: Response) {}
   deleteUser(req: Request, res: Response) {}
   findUserByEmail(req: Request, res: Response) {}
