b879a3edc13d22f1360e0f763ca664765c48e4db
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-09-11 16:27:05 +0100

    improvement: re-write request interceptors

--- frontend/src/api/interceptors/request.interceptor.ts
+++ frontend/src/api/interceptors/request.interceptor.ts
@@ -1,12 +1,25 @@
-import { AxiosRequestConfig } from "axios";
-import getUserAccessToken from "../services/getUserAccessToken";
+import { AxiosError, AxiosRequestConfig } from "axios";
+import useAppStore from "../../store";
 
-const requestInterceptor = (config: AxiosRequestConfig<any>) => {
-  const accessToken = getUserAccessToken();
+/**
+ * Called before request is sent
+ */
+const beforeRequest = (config: AxiosRequestConfig<any>) => {
+  const accessToken = useAppStore.getState().accessToken;
   if (accessToken && config.headers) {
-    config.headers["Authorization"] = `${accessToken}`; // token from storage wil have `Bearer ` prefix
+    config.headers["Authorization"] = `Bearer ${accessToken}`; // token from storage wil have `Bearer ` prefix
   }
   return config;
 };
 
-export default requestInterceptor;
+/**
+ * Called when an error occured before request is sent, maybe bad config
+ */
+const requestError = (error: AxiosError) => {
+  return Promise.reject({
+    message: error.message || "An error occured",
+    howToFix: "Please try again",
+  });
+};
+
+export { beforeRequest, requestError };
