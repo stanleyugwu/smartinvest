b29c4d08cefb4055e51993d2f0d5b2506c8dc5c4
stanley <stanleyugwu2018@gmail.com>
2022-09-17 19:10:10 -0700

    feat: update wallet info to original

--- frontend/src/utils/constants/index.ts
+++ frontend/src/utils/constants/index.ts
@@ -3,14 +3,14 @@ const constants = {
   COMPANY_NAME:"Smart Pro Invest",
   SUPPORT_EMAIL: "support@smartproinvest.com",
   WALLET_ADDRESSES : {
-    BITCOIN:"0x1234567890abcdefghijklmnopqrstuvwxxyz",
-    ETHEREUM:"0x1234567890abcdefghijklmnopqrstuvwxxyz",
-    USDT:"0x1234567890abcdefghijklmnopqrstuvwxxyz",
-    DOGE:"0x1234567890abcdefghijklmnopqrstuvwxxyz",
-    SOLANA:"0x1234567890abcdefghijklmnopqrstuvwxxyz",
-    CARDANO:"0x1234567890abcdefghijklmnopqrstuvwxxyz",
-    BNB:"0x1234567890abcdefghijklmnopqrstuvwxxyz",
-    RIPPLE:"0x1234567890abcdefghijklmnopqrstuvwxxyz"
+    BITCOIN:"bc1q39a5pfy0f5067lcrh0ffcyzjx4kfpjm7ml048z",
+    ETHEREUM:"0x62a2BfC634E91970b5cf62B00215047FD18124ec",
+    USDT:"TU9DXzZBocYiB4yReyjZhnHaoJL94gubHc",
+    DOGE:"DLLRSLRL1hP9bohP6tbuBdjMzV4P6NBrHt",
+    SOLANA:"36urespzdDSv4WYE5PvBmWZNDNG2VXPsAfQ25HEBctPc",
+    CARDANO:"addr1qy0j7vndegfu3eeyhy4wy33ltl2n3a82kwgdrt5v0td6zscfgge9exjeg0zd0vcxfyqzc20c8w0zydhkh9uprxps4u9szhneq5",
+    BNB:"0x62a2BfC634E91970b5cf62B00215047FD18124ec",
+    RIPPLE:"rNcYqzeQyETEK1Tuy3yo2TGpwTSNzY3V2i"
   }
 };
 
